1613e41b91e9cc215d37b1af09272f1e74bd973c
unknown <hamaday468@gmail.com>
2025-01-25 23:11:34 +0200

    make almost the entire product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,60 +6,68 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, price, description, name } = product;
-  console.log(product);
+  console.log(products);
 
   return (
     <div>
       <div className="product-detail-container">
-        <div>
-          <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="" />
-          </div>
-          {/* <div className="small-images-container">
+        <div className="image-container">
+          <img src={urlFor(image && image[0])} alt="" />
+        </div>
+        {/* <div className="small-images-container">
             {image?.map((item, i) => (
               <img src={urlFor(item)} className="" onMouseEnter=""/>
             ))}
           </div> */}
 
-          <div className="product-detail-desc">
-            <h1>{name}</h1>
-            <div className="reviews">
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiOutlineStar />
-            </div>
-            <p>(20)</p>
-            <h4>Details: </h4>
-            <p>{description}</p>
-            <p className="price">${price}</p>
-            <div className="quantity">
-              <h3>Quantity: </h3>
-              <p className="quantity-desc">
-                <span className="minus">
-                  {" "}
-                  <AiOutlineMinus />{" "}
-                </span>
-                <span className="num">0</span>
-                <span className="plus">
-                  {" "}
-                  <AiOutlinePlus />
-                </span>
-              </p>
-            </div>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiOutlineStar />
+          </div>
+          <p>(20)</p>
+          <h4>Details: </h4>
+          <p>{description}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">0</span>
+              <span className="plus">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
 
-            <div className="buttons">
-              <button className="add-to-cart" type="button">
-                Add to Cart
-              </button>
-              <button className="buy-now" type="button">
-                Buy Now
-              </button>
-            </div>
+          <div className="buttons">
+            <button className="add-to-cart" type="button">
+              Add to Cart
+            </button>
+            <button className="buy-now" type="button">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>you may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
           </div>
         </div>
       </div>
